9b2d0c6f5e0603ea479684b94228bb3098d37df3
Luiz Alberto Fonseca <luiz.fonseca@ccc.ufcg.edu.br>
2022-03-08 12:12:01 +0100

    Add code for creating the edges

--- PartA.2_FonsecaRepas.py
+++ PartA.2_FonsecaRepas.py
@@ -62,11 +62,77 @@ class App:
     @staticmethod
     def _load_edges(tx):
         query = (
+            "// Wrote"
             "LOAD CSV WITH HEADERS FROM 'file:///wrote.csv' AS row "
             "MATCH (a:Author {id: row.authorid})"
             "MATCH (p:Paper {id: row.paperid})"
             "MERGE (a)-[:Wrote]->(p);"
-            # TODO: put the rest of the edges here once we have the data
+            
+            "// Reviewed"
+            "LOAD CSV WITH HEADERS FROM 'file:///reviwed.csv' AS row "
+            "MATCH (a:Author {id: row.authorid})"
+            "MATCH (p:Paper {id: row.paperid})"
+            "MERGE (a)-[:Reviewed]->(p);"
+
+            "// Corresponding"
+            "LOAD CSV WITH HEADERS FROM 'file:///corresponding.csv' AS row "
+            "MATCH (a:Author {id: row.authorid})"
+            "MATCH (p:Paper {id: row.paperid})"
+            "MERGE (a)-[:Corresponding]->(p);"
+
+            "// Published_in (author - edition)"
+            "LOAD CSV WITH HEADERS FROM 'file:///published_in_edition.csv' AS row "
+            "MATCH (a:Author {id: row.authorid})"
+            "MATCH (e:Edition {id: row.editionid})"
+            "MERGE (a)-[:Published_in]->(e);"
+
+            "// Has (paper - keyword)"
+            "LOAD CSV WITH HEADERS FROM 'file:///has_keyword.csv' AS row "
+            "MATCH (p:Paper {id: row.paperid})"
+            "MATCH (k:Keyword {id: row.keywordid})"
+            "MERGE (p)-[:Has]->(k);"
+
+            "// Cites"
+            "LOAD CSV WITH HEADERS FROM 'file:///cites.csv' AS row "
+            "MATCH (p1:Paper {id: row.paper1id})"
+            "MATCH (p2:Paper {id: row.paper2id})"
+            "MERGE (p1)-[:Cites]->(p2);"
+
+            "// Has (proceeding - edition)"
+            "LOAD CSV WITH HEADERS FROM 'file:///has_edition.csv' AS row "
+            "MATCH (p:Proceeding {id: row.proceedingid})"
+            "MATCH (e:Edition {id: row.editionid})"
+            "MERGE (p)-[:Has]->(e);"
+
+            "// Happened_in"
+            "LOAD CSV WITH HEADERS FROM 'file:///happened_in.csv' AS row "
+            "MATCH (e:Edition {id: row.editionid})"
+            "MATCH (y:Year {id: row.year})"
+            "MERGE (e)-[:Happened_in]->(y);"
+
+            "// Published_in (volume - year)"
+            "LOAD CSV WITH HEADERS FROM 'file:///published_in_year.csv' AS row "
+            "MATCH (v:Volume {id: row.volumeid})"
+            "MATCH (y:Year {id: row.year})"
+            "MERGE (v)-[:Published_in]->(y);"
+
+            "// Contains"
+            "LOAD CSV WITH HEADERS FROM 'file:///contains.csv' AS row "
+            "MATCH (v:Volume {id: row.volumeid})"
+            "MATCH (p:Paper {id: row.year})"
+            "MERGE (v)-[:Contains]->(p);"
+
+            "// Has (journal - volume)"
+            "LOAD CSV WITH HEADERS FROM 'file:///has_volume.csv' AS row "
+            "MATCH (j:Journal {id: row.journalid})"
+            "MATCH (v:Volume {id: row.volumeid})"
+            "MERGE (j)-[:Has]->(v);"
+
+            "// Published_in (paper - year)"
+            "LOAD CSV WITH HEADERS FROM 'file:///paper_published_in_year.csv' AS row "
+            "MATCH (p:Paper {id: row.paperid})"
+            "MATCH (y:Year {id: row.year})"
+            "MERGE (p)-[:Published_in]->(y);"
         )
         tx.run(query)
     
@@ -78,5 +144,6 @@ if __name__ == "__main__":
     password = "sdm123"
     App.enable_log(logging.INFO, sys.stdout)
     app = App(bolt_url, user, password)
-    app.load_companies()
+    app.load_nodes()
+    app.load_edges()
     app.close()
