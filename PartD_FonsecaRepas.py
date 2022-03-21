760fab35aadef9925f2c953b624c0d16ceecf786
Luiz Alberto Fonseca <luiz.fonseca@ccc.ufcg.edu.br>
2022-03-21 12:56:45 +0100

    Add part 2 of question D

--- PartD_FonsecaRepas.py
+++ PartD_FonsecaRepas.py
@@ -25,7 +25,6 @@ class App:
             session.write_transaction(self._create_database_community)
             session.write_transaction(self._connect_community_keywords)
             
-
     @staticmethod
     def _create_database_community(tx):
         query = ("CREATE (:Community {name: 'database'});")
@@ -40,6 +39,39 @@ class App:
             "CREATE (community)-[:Contains]->(keyword); "
             )
         tx.run(query)
+
+    def relate_journals_conferences_to_community(self):
+        with self.driver.session() as session:
+            session.write_transaction(self._relate_conferences_to_community)
+            session.write_transaction(self._relate_journals_to_community)
+
+    @staticmethod
+    def _relate_conferences_to_community(tx):
+        query = (
+            "MATCH (conference:Conference)-[:Has]->(:Edition)<-[:Published_in]-(paper:Paper) "
+            "WITH conference, COUNT (paper) AS total_papers "
+            "MATCH (conference)-[:Has]->(:Edition)<-[:Published_in]-(community_paper:Paper)-[:Has]->(:Keyword)<-[:Contains]-(:Community {name: 'database'}) "
+            "WITH conference, COUNT(community_paper) AS total_com_papers, total_papers "
+            "WITH conference, total_com_papers * 1.0 / total_papers AS community_percentual"
+            "WHERE community_percentual >= 0.03 "
+            "MATCH (community:Community {name: 'database'}) "
+            "CREATE (conference)-[:Relates]->(community); "
+            )
+        tx.run(query)
+
+    @staticmethod
+    def _relate_journals_to_community(tx):
+        query = (
+            "MATCH (journal:Journal)-[:Has]->(:Volume)-[:Contains]->(paper:Paper) "
+            "WITH journal, COUNT (paper) AS total_papers "
+            "MATCH (journal:Journal)-[:Has]->(:Volume)-[:Contains]->(community_paper:Paper)-[:Has]->(:Keyword)<-[:Contains]-(:Community {name: 'database'}) "
+            "WITH journal, COUNT(community_paper) AS total_com_papers, total_papers "
+            "WITH journal, total_com_papers * 1.0 / total_papers AS community_percentual "
+            "WHERE community_percentual >= 0.03 "
+            "MATCH (community:Community {name: 'database'}) "
+            "CREATE (journal)-[:Relates]->(community); "
+            )
+        tx.run(query)
         
 
 if __name__ == "__main__":
@@ -49,4 +81,5 @@ if __name__ == "__main__":
     App.enable_log(logging.INFO, sys.stdout)
     app = App(bolt_url, user, password)
     app.create_database_community()
+    app.relate_journals_conferences_to_community()
     app.close()
